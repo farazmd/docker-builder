0953a028ed96b2ab741bb835154fc5ceb889b821
Mohammad Faraz <farazm708@gmail.com>
2021-09-08 17:26:11 +0530

    Added functionality to build images and write logs to file

--- docker-builder.py
+++ docker-builder.py
@@ -6,13 +6,26 @@ import json
 
 CONFIG_FILE = None
 DOCKERFILE = None
+CLIENT = None
 
 parser = argparse.ArgumentParser(prog='docker-builder',description='Helps to build and push docker images')
 parser.add_argument('--file',help='Path to docker file',required=True)
 parser.add_argument('--config',help='Path to configuration file',required=True)
 
 
+def createDockerClient():
+    '''
+        Returns docker client object.
+    '''
+    try:
+        return docker.from_env()
+    except Exception as e:
+        raise RuntimeError
+
 def checkValidConfig(config):
+    '''
+        Checks if the config file is valid.
+    '''
     if 'name' not in config:
         raise AttributeError
     elif 'tag' not in config:
@@ -36,7 +49,7 @@ def readConfig(configFile):
         if 'deploy' in config:
             deploy = config['deploy']
         else:
-            deploy = False
+            deploy = None
         return imageName,tag,deploy
     except Exception as e:
         print("Error")
@@ -50,14 +63,80 @@ def checkFileExists(file):
     if not os.path.exists(file):
         raise FileNotFoundError
     else:
-        return file
+        return os.path.abspath(file)
+
+
+def dockerHubLogin():
+    '''
+        Helps to authenticate to docker hub.
+    '''
+    if 'HUB_USERNAME' in os.environ and 'HUB_PASS' in os.environ:
+        try:
+            CLIENT.login(username=os.environ['HUB_USERNAME'],password=os.environ['HUB_PASS'])
+        except Exception as e:
+            print(e.__cause__)
+    else:
+        raise AttributeError
+
+
+def dockerCheck():
+    '''
+        Checks if docker is present in the environment.
+    '''
+    return None
+
+
+def writeLogstoFile(logs,createdImage):
+    '''
+        Helps to write build logs to file.
+    '''
+    try:
+        home_dir = os.path.expanduser("~")
+        logs_dir = os.path.join(home_dir,".docker_builder_logs")
+        if not os.path.exists(logs_dir):
+            os.makedirs(logs_dir)
+        with open(f"{logs_dir}{os.path.sep}{createdImage}-logs",'w') as f:
+            for line in logs:
+                if 'stream' in line:
+                    f.write(line['stream'])
+        print(f"Successfully wrote build logs to {logs_dir}{os.path.sep}{createdImage}-logs")
+    except Exception as e:
+        print("Failed to write logs to file")
+
+
+def buildImage(imageName,tagName):
+    '''
+        Helps to build the image.
+    '''
+    try:
+        with open(DOCKERFILE,'rb') as f:
+            try:
+                result = CLIENT.images.build(
+                    fileobj = f,
+                    tag = f'{imageName}:{tagName}',
+                    forcerm=True
+                )
+                writeLogstoFile(result[1],f'{imageName}-{tagName}')
+                print(f"Successfully built image with ID: {result[0].id} and tags: {result[0].attrs['RepoTags']}") 
+            except Exception as e:
+                print('Failed to build image.')
+                sys.exit(1)
+    except Exception as e:
+        print(f"Failed to not open dockerfile: {DOCKERFILE}")
+        sys.exit(1)
 
 
 def main():
     '''
         Program starts here.
     '''
+    global CLIENT,DOCKERFILE,CONFIG_FILE
     arguments = parser.parse_args()
+    try:
+        CLIENT = createDockerClient()
+    except RuntimeError:
+        print('Docker is not running or not installed')
+        sys.exit(1)
     try:
         DOCKERFILE = checkFileExists(arguments.file)
     except FileNotFoundError:
@@ -69,7 +148,8 @@ def main():
         print(f'Configuration not found at {arguments.config}')
         sys.exit(1)
     imageName,tag,deploy = readConfig(CONFIG_FILE)
-    print(imageName,tag,deploy)
+    buildImage(imageName,tag)
+    
 
 if __name__ == "__main__":
     main()
