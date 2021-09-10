ef4b5754e475fd3d81b16359fd76824983ed7040
Mohammad Faraz <farazm708@gmail.com>
2021-09-10 23:23:22 +0530

    1. Renamed file 2. Added tests for parser

---
+++ tests/parser/test_parser.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import pytest
+sys.path.append(os.path.pathsep.join(__file__.split()[:-2]))
+import docker_builder
+
+''' Testing parser functionality'''
+
+def test_valid_parser_arguments():
+    parser = docker_builder.createParser()
+    arguments = parser.parse_args(['--file','./files/Dockerfile.sample','--config','./files/sample_config.json'])
+    assert len(vars(arguments)) == 2
+
+def test_no_config_passed(capsys):
+    parser = docker_builder.createParser()
+    with pytest.raises(SystemExit):
+        parser.parse_args(['--file','./files/Dockerfile.sample'])
+    captured = capsys.readouterr()
+    assert 'error: the following arguments are required: --config' in captured.err
+
+def test_no_file_passed(capsys):
+    parser = docker_builder.createParser()
+    with pytest.raises(SystemExit):
+        parser.parse_args(['--config','./files/sample_config.json'])
+    captured = capsys.readouterr()
+    assert 'error: the following arguments are required: --file' in captured.err
+
+def test_no_arguments_passed(capsys):
+    parser = docker_builder.createParser()
+    with pytest.raises(SystemExit):
+        parser.parse_args([])
+    captured = capsys.readouterr()
+    assert 'error: the following arguments are required: --file, --config' in captured.err
