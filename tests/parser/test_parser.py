4c22d5eee44a7c7b0f998f03960125b630c54411
Mohammad Faraz <farazm708@gmail.com>
2021-09-10 23:24:34 +0530

    Updated file values

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -8,20 +8,20 @@ import docker_builder
 
 def test_valid_parser_arguments():
     parser = docker_builder.createParser()
-    arguments = parser.parse_args(['--file','./files/Dockerfile.sample','--config','./files/sample_config.json'])
+    arguments = parser.parse_args(['--file','./files/Dockerfile.test','--config','./files/test_config.json'])
     assert len(vars(arguments)) == 2
 
 def test_no_config_passed(capsys):
     parser = docker_builder.createParser()
     with pytest.raises(SystemExit):
-        parser.parse_args(['--file','./files/Dockerfile.sample'])
+        parser.parse_args(['--file','./files/Dockerfile.test'])
     captured = capsys.readouterr()
     assert 'error: the following arguments are required: --config' in captured.err
 
 def test_no_file_passed(capsys):
     parser = docker_builder.createParser()
     with pytest.raises(SystemExit):
-        parser.parse_args(['--config','./files/sample_config.json'])
+        parser.parse_args(['--config','./files/test_config.json'])
     captured = capsys.readouterr()
     assert 'error: the following arguments are required: --file' in captured.err
 
